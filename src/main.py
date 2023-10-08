d93bdd1336d6db0f6956c982c712312cdfa9f7a6
MSCI\gokhvis <vishal.gokhale@msci.com>
2023-10-08 21:04:38 +0530

    Working stage 3 - completed for columns as well

--- src/main.py
+++ src/main.py
@@ -3,11 +3,11 @@ from copy import deepcopy
 import numpy as np
 import pandas as pd
 
-from candidate_cells_for_digit import candidate_cells_for_digit, update_with_candidate_cells_for_digit_method, \
-    row_neighbors
+from candidate_cells_for_digit import candidate_cells_for_digit, update_with_candidate_cells_for_digit_method
 from candidate_digits_for_cell import update_with_candidate_digits_for_cell_method
 from helper import read_input
 from linear_alignment import remove_candidates_impacted_by_linear_alignment
+from stage3 import remove_candidates_by_rows, remove_candidates_by_cols
 
 
 def update_arr(arr, candidates):
@@ -16,26 +16,6 @@ def update_arr(arr, candidates):
             if len(cells) == 1:
                 cell = cells[0]
                 arr[cell[0], cell[1]] = digit
-
-
-def remove_candidates_by_rows_columns(candidates, arr):
-    for digit in range(1, 10):
-        for (smr, smc), cells in candidates[digit].items():
-            if digit == 3 and smr == 0 and smc == 2:
-                print(f"{arr[8, 1]=}")
-            for c in cells:
-                # row_coord = c[0]
-                other_cells_in_same_row = [x for x in cells if x[0] == c[0] and x[1] != c[1]]
-                if not other_cells_in_same_row:
-                    r_neighs = row_neighbors(smr, smc)
-                    candidate_cells_neigh_sub_mat_in_same_row = []
-                    for neigh in r_neighs:
-                        if neigh in candidates[digit] and candidates[digit][neigh]:
-                            candidate_cells_neigh_sub_mat_in_same_row += [x for x in candidates[digit][neigh] if
-                                                                          x[0] == c[0]]
-                    if candidate_cells_neigh_sub_mat_in_same_row:
-                        continue
-                    arr[c[0], c[1]] = digit
 
 
 if __name__ == '__main__':
@@ -53,7 +33,10 @@ if __name__ == '__main__':
         remove_candidates_impacted_by_linear_alignment(candidates)
         update_arr(arr, candidates)
         candidates = candidate_cells_for_digit(arr)
-        remove_candidates_by_rows_columns(candidates, arr)
+        remove_candidates_by_rows(candidates, arr)
+        candidates = candidate_cells_for_digit(arr)
+        remove_candidates_by_cols(candidates, arr)
+
 
         if np.array_equal(arr_copy, arr):
             print(f"{iterations = } completed\n\nNo updates compared to previous state, exiting...")
---
+++ src/stage3.py
@@ -0,0 +1,41 @@
+from candidate_cells_for_digit import row_neighbors, col_neighbors
+
+
+def remove_candidates_by_rows(candidates, arr):
+    for digit in range(1, 10):
+        for (smr, smc), cells in candidates[digit].items():
+            if digit == 3 and smr == 0 and smc == 2:
+                print(f"{arr[8, 1]=}")
+            for c in cells:
+                # row_coord = c[0]
+                other_cells_in_same_row = [x for x in cells if x[0] == c[0] and x[1] != c[1]]
+                if not other_cells_in_same_row:
+                    r_neighs = row_neighbors(smr, smc)
+                    candidate_cells_neigh_sub_mat_in_same_row = []
+                    for neigh in r_neighs:
+                        if neigh in candidates[digit] and candidates[digit][neigh]:
+                            candidate_cells_neigh_sub_mat_in_same_row += [x for x in candidates[digit][neigh] if
+                                                                          x[0] == c[0]]
+                    if candidate_cells_neigh_sub_mat_in_same_row:
+                        continue
+                    arr[c[0], c[1]] = digit
+
+
+def remove_candidates_by_cols(candidates, arr):
+    for digit in range(1, 10):
+        for (smr, smc), cells in candidates[digit].items():
+            if digit == 3 and smr == 0 and smc == 2:
+                print(f"{arr[8, 1]=}")
+            for c in cells:
+                # row_coord = c[0]
+                other_cells_in_same_col = [x for x in cells if x[1] == c[1] and x[0] != c[0]]
+                if not other_cells_in_same_col:
+                    c_neighs = col_neighbors(smr, smc)
+                    candidate_cells_neigh_sub_mat_in_same_col = []
+                    for neigh in c_neighs:
+                        if neigh in candidates[digit] and candidates[digit][neigh]:
+                            candidate_cells_neigh_sub_mat_in_same_col += [x for x in candidates[digit][neigh] if
+                                                                          x[1] == c[1]]
+                    if candidate_cells_neigh_sub_mat_in_same_col:
+                        continue
+                    arr[c[0], c[1]] = digit
